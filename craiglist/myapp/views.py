cdecec1e11dd378a93fd3904c89b82e2244e3808
Vladmir-dev <tempestpius70@gmail.com>
2021-03-24 14:47:15 +0300

    first commit

---
+++ craiglist/myapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
+
+# Create your views here.
+def home(request):
+    return render(request, 'base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    context = {'search':search}
+    return render(request, 'myapp/new_search.html', context)
